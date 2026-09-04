365bd0478b2a51f97c936b8c1ce114a1090d023e
CLiun <chengnan.liu39@gmail.com>
2026-09-05 07:03:17 +0900

    done lab02

--- Lab/lab02/vector.c
+++ Lab/lab02/vector.c
@@ -58,27 +58,30 @@ vector_t *vector_new() {
     vector_t *retval;
 
     /* First, we need to allocate memory on the heap for the struct */
-    retval = /* YOUR CODE HERE */
+    retval = malloc(sizeof(vector_t));/* YOUR CODE HERE */
 
     /* Check our return value to make sure we got memory */
-    if (/* YOUR CODE HERE */) {
+    if (retval == NULL) {
         allocation_failed();
     }
 
     /* Now we need to initialize our data.
        Since retval->data should be able to dynamically grow,
        what do you need to do? */
-    retval->size = /* YOUR CODE HERE */;
-    retval->data = /* YOUR CODE HERE */;
+    retval->size = 100;/* YOUR CODE HERE */;
+    retval->data = malloc(retval->size * sizeof(int));/* YOUR CODE HERE */;
 
     /* Check the data attribute of our vector to make sure we got memory */
-    if (/* YOUR CODE HERE */) {
+    if (retval->data == NULL) {
         free(retval);				//Why is this line necessary?
         allocation_failed();
     }
 
     /* Complete the initialization by setting the single component to zero */
-    /* YOUR CODE HERE */ = 0;
+		for (int i = 0; i < retval->size; i++) {
+			retval->data[i] = 0;
+		}
+
 
     /* and return... */
     return retval;
@@ -96,8 +99,8 @@ int vector_get(vector_t *v, size_t loc) {
     /* If the requested location is higher than we have allocated, return 0.
      * Otherwise, return what is in the passed location.
      */
-    if (loc < /* YOUR CODE HERE */) {
-        return /* YOUR CODE HERE */;
+    if (loc < v->size/* YOUR CODE HERE */) {
+        return v->data[loc];
     } else {
         return 0;
     }
@@ -106,6 +109,8 @@ int vector_get(vector_t *v, size_t loc) {
 /* Free up the memory allocated for the passed vector.
    Remember, you need to free up ALL the memory that was allocated. */
 void vector_delete(vector_t *v) {
+		free(v->data);
+		free(v);
     /* YOUR SOLUTION HERE */
 }
 
@@ -117,4 +122,16 @@ void vector_set(vector_t *v, size_t loc, int value) {
      */
 
     /* YOUR SOLUTION HERE */
+		if (v->size <= loc) {
+			v->data = realloc(v->data, (loc + 1) * sizeof(int));
+			if (v->data == NULL) {
+        fprintf(stderr, "vector_get: passed a NULL vector.\n");
+				abort();
+			}
+			for (int i = v->size; i < loc; i++) {
+				v->data[i] = 0;
+			}
+			v->size = loc + 2;
+		}
+		v->data[loc] = value;
 }
