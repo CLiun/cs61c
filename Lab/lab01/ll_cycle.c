9e540d47c7710b85863fdd547c18f8bea5e98c1a
CLiun <chengnan.liu39@gmail.com>
2026-09-05 01:29:43 +0900

    done lab01

--- Lab/lab01/ll_cycle.c
+++ Lab/lab01/ll_cycle.c
@@ -3,5 +3,17 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *tortoise = head;
+    node *hare = head;
+    if (hare == NULL || hare->next == NULL) {
+        return 0;
+    }
+    while (hare != NULL && hare->next != NULL) {
+        hare = hare->next->next;
+        tortoise = tortoise->next;
+        if (tortoise == hare) {
+            return 1;
+        }
+    }
     return 0;
 }
--- Lab/lab01/segfault_ex.c
+++ Lab/lab01/segfault_ex.c
@@ -1,6 +1,9 @@
+#include <stdlib.h>
+
 int main() {
-    int a[20];
-    for (int i = 0; ; i++) {
+    int *a = malloc(10 * sizeof(int));
+    for (int i = 0; i < 10; i++) {
         a[i] = i;
     }
+    free(a);
 }
